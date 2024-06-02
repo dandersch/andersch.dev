29315014a34772da58636a905f827e2d99fb9232
da <>
2024-06-02 11:40:34 +0200

    add serve.sh script

---
+++ serve.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import http.server
+import socketserver
+
+PORT = 1337
+
+class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    extensions_map = {
+        '':      'text/html',
+        '.html': 'text/html',
+        '.png':  'image/png',
+        '.jpg':  'image/jpg',
+        '.svg':	 'image/svg+xml',
+        '.css':	 'text/css',
+    }
+
+httpd = socketserver.TCPServer(("localhost", PORT), HttpRequestHandler)
+
+try:
+    print(f"serving at http://localhost:{PORT}")
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    httpd.shutdown()
+    pass
